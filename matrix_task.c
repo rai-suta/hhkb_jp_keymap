d4ed32963891903163c86930a0ed6c5cd9526fec
rai-suta <sn0425.flv+github@gmail.com>
2017-07-05 21:34:53 +0900

    delete next_key_down_up

--- matrix_task.c
+++ matrix_task.c
@@ -55,12 +55,6 @@ void matrix_scan_user(void)
   }
 }
 
-enum next_key_down_up {
-    NK_DOWN_UP,
-    NK_DOWN,
-    NK_UP // a bit of a hack, this works as long as NK_UP < KC_A
-};
-
 #ifdef JIS_KEYCODE
 # define TAP_CAPSLOCK() action_macro_play( MACRO( D(LSFT), T(CAPSLOCK), U(LSFT), END ))
 #else
